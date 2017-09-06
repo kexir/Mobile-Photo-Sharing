50964d9b91b705bbfa7721e28368f89efd09286d
kexir <qlyu@usc.edu>
2017-09-06 11:36:45 -0700

    Initial Commit

---
+++ Mobile-Photo-Sharing/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Mobile-Photo-Sharing
+//
+//  Created by LyuQi on 9/6/17.
+//  Copyright © 2017 LyuQi. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
